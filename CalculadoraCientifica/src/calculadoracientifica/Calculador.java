4071542fcbdc938a057e243523a7156a67806517
estudiantes <estudiantes@labing00>
2017-03-24 17:57:43 -0500

    Se han ingresado los algoritmos para calcular las funciones trigonometricas.

--- CalculadoraCientifica/src/calculadoracientifica/Calculador.java
+++ CalculadoraCientifica/src/calculadoracientifica/Calculador.java
@@ -1,9 +1,14 @@
 package calculadoracientifica;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.tan;
+
 public class Calculador {
 
     public float Numero1;
     public float Numero2;
+    public float Numero3;
     public float Resultado;
     
     public float Sumar(){
@@ -31,6 +36,34 @@ public class Calculador {
         Resultado=Numero1/Numero2;
         return Resultado;
     
+    }
+       
+    public float Seno(){
+        return (float) (sin(Numero3* Math.PI/180.0));
+    }
+    
+    public float Coseno(){
+        return (float) (cos(Numero3* Math.PI/180.0));
+    }
+    
+    public float Tangente(){
+        return (float) (tan(Numero3* Math.PI/180.0));
+    }
+    
+    public float Cotangente(){
+        return (float) (1/tan(Numero3* Math.PI/180.0));
+    }
+    
+    public float Secante(){
+        return (float) (1/cos(Numero3* Math.PI/180.0));
+    }
+    
+    public float Cosecante(){
+        return (float) (1/sen(Numero3* Math.PI/180.0));
+    }
+
+    private int sen(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
        
     
